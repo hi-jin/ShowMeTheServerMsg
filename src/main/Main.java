9c9830c9ab566ba1cc30aaa64d41893c0a56db61
hi-jin <crushed7@o.cnu.ac.kr>
2019-12-20 15:23:09 +0900

    fix main method

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,11 +10,11 @@ public class Main {
 		System.out.println("웹에 원하는 메시지를 띄우는 프로그램입니다.");;
 		int selection;
 		Scanner in = new Scanner(System.in);
+		System.out.println("현재 메시지");
+		FileIO.showMsg();
+		System.out.println("------------------------------------\n어떤 작업을 하시겠습니까?");
+		System.out.println("0. 종료 // 1. 메시지 추가 // 2. 메시지 삭제 // 3. 메시지 전체 삭제 //");
 		while((selection = in.nextInt()) != 0) {
-			System.out.println("현재 메시지");
-			FileIO.showMsg();
-			System.out.println("------------------------------------\n어떤 작업을 하시겠습니까?");
-			System.out.println("0. 종료 // 1. 메시지 추가 // 2. 메시지 삭제 // 3. 메시지 전체 삭제 //");
 			switch(selection) {
 			case 1:
 				System.out.println("추가할 메시지를 입력하세요.");
@@ -32,7 +32,12 @@ public class Main {
 				break;
 			default:
 				System.out.println("0~3의 숫자만 입력하세요");
+				break;
 			}
+			System.out.println("현재 메시지");
+			FileIO.showMsg();
+			System.out.println("------------------------------------\n어떤 작업을 하시겠습니까?");
+			System.out.println("0. 종료 // 1. 메시지 추가 // 2. 메시지 삭제 // 3. 메시지 전체 삭제 //");
 		}
 		in.close();
 		FileIO.writeMsg();
